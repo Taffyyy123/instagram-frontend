57124f400bf3c886692748cd47805acc82c2f57b
Taffyyy123 <tsolmn9@gmail.com>
2025-01-10 23:35:43 +0800

    follow search hagas dutuu duusgarai

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,32 @@
+"use client";
 import { Input } from "@/components/ui/input";
 import BottomNav from "@/custom-components/BottomNav";
+import { userType } from "@/custom-components/isLiked";
+import { useEffect, useState } from "react";
 
 const Page = () => {
+  const [users, setUsers] = useState<userType[]>([]);
+  const getUsers = async () => {
+    const token = localStorage.getItem("accessToken");
+    const jsonData = await fetch(
+      "https://instagram-backend-e3eq.onrender.com/user/getAllUser",
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const response = await jsonData.json();
+    setUsers(response);
+  };
+  useEffect(() => {
+    getUsers;
+  }, []);
+  useEffect(() => {
+    console.log(users);
+  }, [users]); //
   return (
     <div className="w-screen h-screen bg-black flex flex-col items-center">
       <div className="text-white font-sans font-bold flex justify-center text-2xl p-3">
